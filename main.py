1a8ed527e687ab1d1c64a553bb6784fcb1b8de55
Jotta-Art-IFSP <112517025+Jotta-Art-IFSP@users.noreply.github.com>
2023-10-20 11:46:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import unittest
 import numeroromano as nr
 
-print('I = ' + str((nr.converte('I'))))
-print('V = ' + str((nr.converte('V'))))
-print('XXII = ' + str((nr.converte('XXII'))))
-print('IX = ' + str((nr.converte('IX'))))
-print('XXIV = ' + str((nr.converte('XXIV'))))
+class TestConverteRomano(unittest.TestCase):
+
+    def test_I(self):
+        self.assertEqual(nr.converte('I'), 1)
+
+    def test_V(self):
+        self.assertEqual(nr.converte('V'), 5)
+
+    def test_II(self):
+        self.assertEqual(nr.converte('II'), 2)
+
+    def test_XXII(self):
+        self.assertEqual(nr.converte('XXII'), 22)
+
+    def test_IX(self):
+        self.assertEqual(nr.converte('IX'), 9)
+
+    def test_XXIV(self):
+        self.assertEqual(nr.converte('XXIV'), 24)
+
+    if __name__ == '__main__':
+        unittest.main()
