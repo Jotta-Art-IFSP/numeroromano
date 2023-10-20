0e11b00240491c99f60ae3afb683b7fade2b958e
Jotta-Art-IFSP <112517025+Jotta-Art-IFSP@users.noreply.github.com>
2023-10-19 21:21:07 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import numeroromano as nr
+
+print('I = ' + str((nr.converte('I'))))
+print('V = ' + str((nr.converte('V'))))
+print('XXII = ' + str((nr.converte('XXII'))))
+print('IX = ' + str((nr.converte('IX'))))
+print('XXIV = ' + str((nr.converte('XXIV'))))
